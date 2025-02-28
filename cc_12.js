524478968d084c3ec287b0ae8f7e23d6ae9af43e
ElNotsniw <winstonjosh010604@gmail.com>
2025-02-27 20:28:06 -0500

    Task 2 - Updated Metric Cards via Array Conversion

--- cc_12.js
+++ cc_12.js
@@ -6,8 +6,7 @@ const dashboardContainerbyID = document.getElementById(`dashboard`)
 
 const dashboardContainerbyQuery = document.querySelector(`dashboard`)
 
-// Creating a metric card to add to the HTML code, which includes an id (revenueCard) and class (metric-card), then finally appending it to the parent node, "dashboard"
-
+// Creating a metric card to add to the HTML code, which includes an id (revenueCard) and class (metric-card).
 const metricCard = document.createElement(`div`)
 
 metricCard.setAttribute(`id`, `revenueCard`)
@@ -17,4 +16,30 @@ metricCard.innerHTML =
 `<h3>Revenue</h3>
 <p>$0</p>`
 
+// Finally, appending it to the parent node, "dashboard".
+
+
 dashboardContainerbyID.appendChild(metricCard)
+
+
+
+
+// Task 2 - Updated Metric Cards via Array Conversion
+
+// Using the .querySelectorAll to select any elements that have "metric-card" as their class.
+
+const metricCards = document.querySelectorAll(`.metric-card`)
+
+// Converting the NodeList into an array using Array.from.
+
+const metricCardsArray = Array.from(metricCards)
+
+// Updating the inner text from the metric card to have the word, "- Updated" next to it and changing the background color of it.
+
+metricCardsArray.forEach((card) => {
+    const title = card.querySelector(`h3`)
+    if (title) {
+        title.innerText = `${title.innerText} - Updated`
+    }
+    card.style.backgroundColor = `#CBC3E3`
+})
